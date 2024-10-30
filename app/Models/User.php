62c7e31a0d7413909e1fa089bda4acb43a4dc745
unknown <mglessa.dev@gmail.com>
2024-10-30 12:08:32 -0300

    criando relacionamentos

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Category;
+use App\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -44,5 +46,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
+
+    public function categories() {
+        return $this->hasMany(Category::class);
     }
 }
